30648266f86543d94880b93593b2e8a2cb37b838
azad <azad.sadr.h@gmail.com>
2022-01-13 15:29:17 +0100

    modify the get_signature_profile function

--- aux.py
+++ aux.py
@@ -10,6 +10,7 @@ def get_phylogeny_counts(M):
     M = M.float()
     return M   
 
+'''
 def get_signature_profile(beta):
     contexts = list(beta.columns[1:])
     signature_names = list(beta.values[:, 0])
@@ -20,6 +21,24 @@ def get_signature_profile(beta):
     # counts: each row represents a signature profile [k X 96] (dtype:torch.tensor)
     # signature_names: list of signature profiles name (dtype:list)
     # contexts: list of mutation features name (dtype:list)
+'''
+
+def get_signature_profile(b):
+    # same functionality as Riccardo's but more clean
+
+    # list of mutation features name
+    mutation_features = b.columns            # dtype:pandas.core.indexes.base.Index
+    mutation_features = list(b.columns)      # dtype:list
+
+    # list of signature profiles name
+    signature_names = b.index                # dtype:pandas.core.indexes.base.Index
+    signature_names = list(b.index)          # dtype:list
+
+    # convert to torch tensor
+    beta = torch.tensor(b.values)            # dtype:torch.Tensor
+
+    return beta, signature_names, mutation_features
+
 
 def get_alpha_beta(params):
     alpha = torch.exp(params["alpha"])
